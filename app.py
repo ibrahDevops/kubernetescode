aabb0bc10bd9c0faaef563683f900dd07436f774
ibrahDevops <96335503+ibrahDevops@users.noreply.github.com>
2022-06-25 14:40:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, IBrah!!!'
+    return 'Please subscribe, like, and comment on this video!!!'
